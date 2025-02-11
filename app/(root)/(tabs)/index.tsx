19bf30c29b2dfdc94126367bfed1174efc144166
Nagy Gabor <anagybogar@gmail.com>
2025-02-11 15:37:48 +0100

    Fetch data, no result etc

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,27 +1,60 @@
 import { Card, FeaturedCard } from "@/app/components/Cards";
 import Filters from "@/app/components/Filters";
+import NoResults from "@/app/components/NoResults";
 import Search from "@/app/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
 import { useGlobalContext } from "@/lib/global-provider";
 import seed from "@/lib/seed";
-import { Link } from "expo-router";
-import { Text, View, Image, TouchableOpacity, FlatList, Button } from "react-native";
+import { useAppwrite } from "@/lib/useAppwrite";
+import { Link, router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
+import { Text, View, Image, TouchableOpacity, FlatList, Button, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { user } = useGlobalContext()
+  const params = useLocalSearchParams<{query?: string; filter?: string}>()
 
+  const { data: latestProperties, loading: latestPropertiesLoading } = useAppwrite({
+    fn: getLatestProperties
+  })
+
+  const { data: properties, loading, refetch } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6
+    }
+  })
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`)
+
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6
+    })
+  }, [params.filter, params.query])
   return (
     <SafeAreaView className="bg-white h-full">
       <Button title="Seed" onPress={seed} />
-      <FlatList data={[1, 2, 3, 4]}
-        renderItem={() => <Card />}
-        keyExtractor={(item) => item.toString()}
+      <FlatList data={properties}
+        renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)}  />}
+        keyExtractor={(item) => item.$id}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />) :
+            <NoResults />
+          
+        }
         ListHeaderComponent={
           <View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
@@ -44,14 +77,20 @@ export default function Index() {
                   <Text className="text-base font-rubik-bold text-primary-300">See all</Text>
                 </TouchableOpacity>
               </View>
-              <FlatList data={[1, 2, 3, 4]}
-                renderItem={() => <FeaturedCard />}
-                keyExtractor={(item) => item.toString()}
-                horizontal
-                bounces={false}
-                showsHorizontalScrollIndicator={false}
-                contentContainerClassName="flex gap-5 mt-5"
-              />
+              {latestPropertiesLoading ?
+              <ActivityIndicator size="large" className="text-primary-300" /> : !latestProperties || latestProperties.length === 0
+              ? <NoResults /> : (
+                <FlatList 
+                  data={latestProperties}
+                  renderItem={({ item }) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)} />}
+                  keyExtractor={(item) => item.$id}
+                  horizontal
+                  bounces={false}
+                  showsHorizontalScrollIndicator={false}
+                  contentContainerClassName="flex gap-5 mt-5"
+                />
+              )
+              }
             </View>
             <View className="flex flex-row items-center justify-between">
               <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
