bd17a773a7d34e7bbb1e5a1c94493e6a10d46ec3
Nagy Gabor <anagybogar@gmail.com>
2025-02-12 10:38:46 +0100

    Explore page

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -23,7 +23,7 @@ export const FeaturedCard = ({ item: { image, rating, name, address, price }, on
         <Text className='text-base font-rubik text-white' numberOfLines={1}>{address}</Text>
         <View className='flex flex-row items-center justify-between w-full'>
           <Text className='text-xl font-rubik-extrabold text-white'>
-            {price}
+            ${price}
           </Text>
           <Image source={icons.heart} className='size-5' />
         </View>
@@ -47,7 +47,7 @@ export const Card = ({item: { image, rating, name, address, price }, onPress}: P
         <Text className='text-xs font-rubik text-black-200'>{address}</Text>
         <View className='flex flex-row items-center justify-between mt-2'>
           <Text className='text-base font-rubik-bold text-primary-300'>
-            {price}
+            ${price}
           </Text>
           <Image source={icons.heart} className='w-5 h-5 mr-2' tintColor="#191d31" />
         </View>
